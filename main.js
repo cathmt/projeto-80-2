57b12cefcda8daa0abf6d4b0bfd6e3a8a7e11e78
cathmt <108365028+cathmt@users.noreply.github.com>
2022-10-13 18:08:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+guestNameArray = []
+function submit(){
+    var guestName = document.getElementById("guestName").value;
+    console.log(guestName)
+    namesofpeople.push(guestName);
+}
+
+function sorting(){
+    guestName.sort()
+    console.log
+}
